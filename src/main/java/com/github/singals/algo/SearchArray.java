e60b39ed33e39ef27c472b313cab7ee0b16e4b0a
Shashank Singal <shashank.singal@go-jek.com>
2020-11-05 20:55:59 +0530

    implement binary search

--- src/main/java/com/github/singals/algo/SearchArray.java
+++ src/main/java/com/github/singals/algo/SearchArray.java
@@ -1,14 +1,29 @@
 package com.github.singals.algo;
 
+import java.util.Arrays;
 import java.util.Objects;
 
 public class SearchArray {
-    public static boolean byLinearSearch(Integer[] arr, Integer element){
+    public static boolean byLinearSearch(Integer[] arr, Integer element) {
         for (Integer arrElement : arr) {
             if (Objects.equals(arrElement, element)) {
                 return true;
             }
         }
         return false;
+    }
+
+    public static boolean byBinarySearch(Integer[] sortedArr, Integer element) {
+        int max = sortedArr.length;
+        if (max == 1) return Objects.equals(element, sortedArr[0]);
+        int mid = max / 2;
+        if (Objects.equals(sortedArr[mid], element)) return true;
+        if (sortedArr[mid] > element) {
+            Integer[] subArr = Arrays.copyOfRange(sortedArr, 0, mid);
+            return byBinarySearch(subArr, element);
+        } else {
+            Integer[] subArr = Arrays.copyOfRange(sortedArr, mid, sortedArr.length);
+            return byBinarySearch(subArr, element);
+        }
     }
 }
--- src/test/java/com/github/singals/algo/SearchArrayTest.java
+++ src/test/java/com/github/singals/algo/SearchArrayTest.java
@@ -18,5 +18,19 @@ class SearchArrayTest {
         assertTrue(SearchArray.byLinearSearch(new Integer[]{10, 7, 9, 2, 6, 5}, 6));
         assertTrue(SearchArray.byLinearSearch(new Integer[]{10, 7, 9, 2, 6, 5}, 5));
         assertFalse(SearchArray.byLinearSearch(new Integer[]{10, 7, 9, 2, 6, 5}, 0));
+        assertFalse(SearchArray.byLinearSearch(new Integer[]{10, 7, 9, 2, 6, 5}, 23));
+    }
+
+    @Test
+    @DisplayName("BinarySearch")
+    void TestSearchByBinarySearch() {
+        assertTrue(SearchArray.byBinarySearch(new Integer[]{5, 8, 12, 14, 17, 22}, 5));
+        assertTrue(SearchArray.byBinarySearch(new Integer[]{5, 8, 12, 14, 17, 22}, 8));
+        assertTrue(SearchArray.byBinarySearch(new Integer[]{5, 8, 12, 14, 17, 22}, 12));
+        assertTrue(SearchArray.byBinarySearch(new Integer[]{5, 8, 12, 14, 17, 22}, 14));
+        assertTrue(SearchArray.byBinarySearch(new Integer[]{5, 8, 12, 14, 17, 22}, 17));
+        assertTrue(SearchArray.byBinarySearch(new Integer[]{5, 8, 12, 14, 17, 22}, 22));
+        assertFalse(SearchArray.byBinarySearch(new Integer[]{5, 8, 12, 14, 17, 22}, 0));
+        assertFalse(SearchArray.byBinarySearch(new Integer[]{5, 8, 12, 14, 17, 22}, 23));
     }
 }
