6288e93d75a361bff91b64e1adaeb92e2821a249
Shashank Singal <shashank.singal@go-jek.com>
2020-11-05 22:24:00 +0530

    impl merge sort

--- src/main/java/com/github/singals/algo/SortArray.java
+++ src/main/java/com/github/singals/algo/SortArray.java
@@ -17,4 +17,40 @@ public class SortArray {
                         }));
         return arr;
     }
+
+    public static int[] byMergeSort(int[] arr) {
+        mergeSort(arr, 0, arr.length - 1);
+        return arr;
+    }
+
+    private static void mergeSort(int[] arr, int l, int r) {
+//        System.out.format("mergeSort-> arr: %s, l: %d, r: %d \n", Arrays.toString(arr), l, r);
+        if (r > l) {
+            // more than 1 element present
+            int m = (l + r) / 2;
+            mergeSort(arr, l, m);
+            mergeSort(arr, m + 1, r);
+
+            merge(arr, l, m, r);
+        }
+    }
+
+    private static void merge(int[] arr, int l, int m, int r) {
+//        System.out.format("merge-> arr: %s, l: %d, m: %d, r: %d \n", Arrays.toString(arr), l, m, r);
+        // merge sub arrays l..m m+1..r
+        for (int i = 0; i <= m; i++) {
+            for (int j = m + 1; j <= r; j++) {
+                if (arr[i] > arr[r]) {
+                    int temp = arr[i];
+                    arr[i] = arr[r];
+                    arr[r] = temp;
+                } else if (arr[i] > arr[j]) {
+                    int temp = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = temp;
+                    break;
+                }
+            }
+        }
+    }
 }
--- src/test/java/com/github/singals/algo/SortArrayTest.java
+++ src/test/java/com/github/singals/algo/SortArrayTest.java
@@ -11,5 +11,15 @@ class SortArrayTest {
     @DisplayName("bubble-sort-test")
     void TestSortByBubbleSort() {
         assertArrayEquals(new int[]{1, 2, 3, 4, 5}, SortArray.byBubbleSort(new int[]{5, 4, 3, 2, 1}));
+        assertArrayEquals(new int[]{6, 7, 8, 9, 10}, SortArray.byBubbleSort(new int[]{10, 9, 8, 7, 6}));
+        assertArrayEquals(new int[]{31, 32, 33, 34, 35}, SortArray.byBubbleSort(new int[]{31, 35, 32, 34, 33}));
+    }
+
+    @Test
+    @DisplayName("merge-sort-test")
+    void TestSortByMergeSort() {
+        assertArrayEquals(new int[]{1, 2, 3, 4, 5}, SortArray.byMergeSort(new int[]{5, 4, 3, 2, 1}));
+        assertArrayEquals(new int[]{6, 7, 8, 9, 10}, SortArray.byMergeSort(new int[]{10, 9, 8, 7, 6}));
+        assertArrayEquals(new int[]{31, 32, 33, 34, 35}, SortArray.byMergeSort(new int[]{31, 35, 32, 34, 33}));
     }
 }
