29b12912114db97b99cd9af1762618285411c6ab
ssingal <singalshashank90@gmail.com>
2023-07-19 22:34:59 +0530

    solve - kids with greatest number of candies

---
+++ src/main/java/com/github/singals/algo/leetcode/seventyFive/KidsWithGreatestNumberOfCandies.java
@@ -0,0 +1,30 @@
+package com.github.singals.algo.leetcode.seventyFive;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * There are n kids with candies. You are given an integer array candies,
+ * where each candies[i] represents the number of candies the ith kid has,
+ * and an integer extraCandies, denoting the number of extra candies that you have.
+ * <p>
+ * Return a boolean array result of length n, where result[i] is true if,
+ * after giving the ith kid all the extraCandies, they will have the greatest
+ * number of candies among all the kids, or false otherwise.
+ * <p>
+ * Note that multiple kids can have the greatest number of candies.
+ */
+public class KidsWithGreatestNumberOfCandies {
+    public static List<Boolean> kidsWithCandies(int[] candies, int extraCandies) {
+        if (candies == null || candies.length == 0) return new ArrayList<>();
+        int mostCandies = -1;
+        for (int i = 0; i < candies.length; i++) {
+            if (candies[i] > mostCandies) mostCandies = candies[i];
+        }
+        List<Boolean> res = new ArrayList<>(candies.length);
+        for (int i = 0; i < candies.length; i++) {
+            res.add((candies[i] + extraCandies) >= mostCandies);
+        }
+        return res;
+    }
+}
---
+++ src/test/java/com/github/singals/algo/leetcode/seventyFive/KidsWithGreatestNumberOfCandiesTest.java
@@ -0,0 +1,47 @@
+package com.github.singals.algo.leetcode.seventyFive;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class KidsWithGreatestNumberOfCandiesTest {
+    @Test
+    @DisplayName("kids-with-candies-case-one")
+    void kidsWithCandiesCaseOne() {
+        int[] candies = new int[]{2, 3, 5, 1, 3};
+        List<Boolean> res = KidsWithGreatestNumberOfCandies.kidsWithCandies(candies, 3);
+
+        assertTrue(res.get(0));
+        assertTrue(res.get(1));
+        assertTrue(res.get(2));
+        assertFalse(res.get(3));
+        assertTrue(res.get(4));
+    }
+
+    @Test
+    @DisplayName("kids-with-candies-case-two")
+    void kidsWithCandiesCaseTwo() {
+        int[] candies = new int[]{4, 2, 1, 1, 2};
+        List<Boolean> res = KidsWithGreatestNumberOfCandies.kidsWithCandies(candies, 1);
+
+        assertTrue(res.get(0));
+        assertFalse(res.get(1));
+        assertFalse(res.get(2));
+        assertFalse(res.get(3));
+        assertFalse(res.get(4));
+    }
+
+    @Test
+    @DisplayName("kids-with-candies-case-three")
+    void kidsWithCandiesCaseThree() {
+        int[] candies = new int[]{12, 1, 12};
+        List<Boolean> res = KidsWithGreatestNumberOfCandies.kidsWithCandies(candies, 10);
+
+        assertTrue(res.get(0));
+        assertFalse(res.get(1));
+        assertTrue(res.get(2));
+    }
+}
