2a7f6c5eebcc213b4c5e0d1329d5e7e5bd4ee53c
ssingal <singalshashank90@gmail.com>
2023-07-20 11:21:12 +0530

    solve - can place flowers

---
+++ src/main/java/com/github/singals/algo/leetcode/seventyFive/CanPlaceFlowers.java
@@ -0,0 +1,37 @@
+package com.github.singals.algo.leetcode.seventyFive;
+
+/**
+ * You have a long flowerbed in which some of the plots are planted,
+ * and some are not. However, flowers cannot be planted in adjacent plots.
+ * <p>
+ * Given an integer array flowerbed containing 0's and 1's,
+ * where 0 means empty and 1 means not empty,
+ * and an integer n, return true if n new flowers can be planted
+ * in the flowerbed without violating the no-adjacent-flowers rule
+ * and false otherwise.
+ */
+public class CanPlaceFlowers {
+    public static boolean canPlaceFlowers(int[] flowerbed, int n) {
+        int totalLength = flowerbed.length;
+        int maxFlowers = (totalLength + 1) / 2;
+        if (n > maxFlowers) return false;
+        int flowersThatCanBePlanted = 0;
+        for (int i = 0; i < totalLength; i++) {
+            if (i == 0) {
+                if ((totalLength == 1 && flowerbed[i] == 0) || (flowerbed[i] == 0 && flowerbed[i + 1] == 0)) {
+                    flowerbed[i] = 1;
+                    flowersThatCanBePlanted++;
+                }
+            } else if (i == (totalLength - 1)) {
+                if (flowerbed[i] == 0 && flowerbed[i - 1] == 0) {
+                    flowerbed[i] = 1;
+                    flowersThatCanBePlanted++;
+                }
+            } else if (flowerbed[i - 1] == 0 && flowerbed[i] == 0 && flowerbed[i + 1] == 0) {
+                flowerbed[i] = 1;
+                flowersThatCanBePlanted++;
+            }
+        }
+        return flowersThatCanBePlanted >= n;
+    }
+}
---
+++ src/test/java/com/github/singals/algo/leetcode/seventyFive/CanPlaceFlowersTest.java
@@ -0,0 +1,85 @@
+package com.github.singals.algo.leetcode.seventyFive;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CanPlaceFlowersTest {
+
+    @Test
+    @DisplayName("can-place-flower-when-flower-bed-is-full")
+    void canPlaceFlowerWhenFlowerBedIsFull() {
+        int[] flowerbed = new int[]{1, 0, 1, 0, 1};
+        boolean res = CanPlaceFlowers.canPlaceFlowers(flowerbed, 1);
+        assertFalse(res);
+
+        flowerbed = new int[]{0, 1, 0, 1, 0};
+        res = CanPlaceFlowers.canPlaceFlowers(flowerbed, 1);
+        assertFalse(res);
+
+        flowerbed = new int[]{0, 1, 0, 1};
+        res = CanPlaceFlowers.canPlaceFlowers(flowerbed, 1);
+        assertFalse(res);
+
+        flowerbed = new int[]{1, 0, 1, 0};
+        res = CanPlaceFlowers.canPlaceFlowers(flowerbed, 1);
+        assertFalse(res);
+
+        flowerbed = new int[]{1};
+        res = CanPlaceFlowers.canPlaceFlowers(flowerbed, 1);
+        assertFalse(res);
+    }
+
+    @Test
+    @DisplayName("can-place-flower-when-there-is-space")
+    void canPlaceFlowerWhenThereIsSpace() {
+        int[] flowerbed = new int[]{1, 0, 0, 0, 1};
+        boolean res = CanPlaceFlowers.canPlaceFlowers(flowerbed, 1);
+        assertTrue(res);
+
+        flowerbed = new int[]{0, 1, 0, 0, 0};
+        res = CanPlaceFlowers.canPlaceFlowers(flowerbed, 1);
+        assertTrue(res);
+
+        flowerbed = new int[]{1, 0, 0, 0};
+        res = CanPlaceFlowers.canPlaceFlowers(flowerbed, 1);
+        assertTrue(res);
+
+        flowerbed = new int[]{0, 1, 0, 0};
+        res = CanPlaceFlowers.canPlaceFlowers(flowerbed, 1);
+        assertTrue(res);
+
+        flowerbed = new int[]{0};
+        res = CanPlaceFlowers.canPlaceFlowers(flowerbed, 1);
+        assertTrue(res);
+    }
+
+    @Test
+    @DisplayName("can-place-flower-when-space-is-lesser")
+    void canPlaceFlowerWhenSpaceIsLesser() {
+        int[] flowerbed = new int[]{1, 0, 0, 0, 1};
+        boolean res = CanPlaceFlowers.canPlaceFlowers(flowerbed, 2);
+        assertFalse(res);
+
+        flowerbed = new int[]{0, 1, 0, 0, 0};
+        res = CanPlaceFlowers.canPlaceFlowers(flowerbed, 2);
+        assertFalse(res);
+
+        flowerbed = new int[]{1, 0, 0, 0};
+        res = CanPlaceFlowers.canPlaceFlowers(flowerbed, 2);
+        assertFalse(res);
+
+        flowerbed = new int[]{0, 1, 0, 0};
+        res = CanPlaceFlowers.canPlaceFlowers(flowerbed, 2);
+        assertFalse(res);
+    }
+
+    @Test
+    @DisplayName("can-place-flower-when-too-many-flowers")
+    void canPlaceFlowerWhenTooManyFlowers() {
+        int[] flowerbed = new int[]{0, 0, 0, 0, 0};
+        boolean res = CanPlaceFlowers.canPlaceFlowers(flowerbed, 4);
+        assertFalse(res);
+    }
+}
