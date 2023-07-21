33c5d4eb6128614b6b096a1c20eb27525e062699
ssingal <singalshashank90@gmail.com>
2023-07-21 19:36:32 +0530

    solve - product of array except self

---
+++ src/main/java/com/github/singals/algo/leetcode/seventyFive/ProductOfArrayExceptSelf.java
@@ -0,0 +1,31 @@
+package com.github.singals.algo.leetcode.seventyFive;
+
+/**
+ * Given an integer array nums, return an array answer such that
+ * answer[i] is equal to the product of all the elements of nums except nums[i].
+ * <p>
+ * The product of any prefix or suffix of nums is guaranteed to fit in a 32-bit integer.
+ * <p>
+ * You must write an algorithm that runs in O(n) time and without using the division operation.
+ */
+public class ProductOfArrayExceptSelf {
+    public static int[] productExceptSelf(int[] nums) {
+        int length = nums.length;
+        int[] res = new int[length];
+        for (int i = 0; i < length; i++) {
+            if (i == 0) {
+                res[i] = 1;
+            } else {
+                res[i] = res[i - 1] * nums[i - 1];
+            }
+        }
+        int postfix = 1;
+        for (int i = (length - 1); i >= 0; i--) {
+            if (i != (length - 1)) {
+                postfix *= nums[i + 1];
+                res[i] = res[i] * postfix;
+            }
+        }
+        return res;
+    }
+}
---
+++ src/test/java/com/github/singals/algo/leetcode/seventyFive/ProductOfArrayExceptSelfTest.java
@@ -0,0 +1,21 @@
+package com.github.singals.algo.leetcode.seventyFive;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ProductOfArrayExceptSelfTest {
+    @Test
+    @DisplayName("product-except-self")
+    void productExceptSelf() {
+        int[] res = ProductOfArrayExceptSelf.productExceptSelf(new int[]{1, 2, 3, 4});
+        assertArrayEquals(new int[]{24, 12, 8, 6}, res);
+
+        res = ProductOfArrayExceptSelf.productExceptSelf(new int[]{-1, 1, 0, -3, 3});
+        assertArrayEquals(new int[]{0, 0, 9, 0, 0}, res);
+
+        res = ProductOfArrayExceptSelf.productExceptSelf(new int[]{3, 4, 5, 6, 7});
+        assertArrayEquals(new int[]{840, 630, 504, 420, 360}, res);
+    }
+}
