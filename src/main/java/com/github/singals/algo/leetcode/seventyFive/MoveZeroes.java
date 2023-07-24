237bbe34e816205fdf3a5b2e53c461c5ec70d0f6
ssingal <singalshashank90@gmail.com>
2023-07-24 17:31:18 +0530

    solve - move zeroes

---
+++ src/main/java/com/github/singals/algo/leetcode/seventyFive/MoveZeroes.java
@@ -0,0 +1,25 @@
+package com.github.singals.algo.leetcode.seventyFive;
+
+/**
+ * Given an integer array nums, move all 0's to the end of it while
+ * maintaining the relative order of the non-zero elements.
+ * <p>
+ * Note that you must do this in-place without making a copy of the array.
+ */
+public class MoveZeroes {
+    public static void moveZeroes(int[] nums) {
+        if (nums.length <= 1) return;
+        int count = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != 0) {
+                if (i != count) {
+                    int tmp = nums[i];
+                    nums[i] = nums[count];
+                    nums[count] = tmp;
+                }
+                count++;
+            }
+        }
+    }
+}
---
+++ src/test/java/com/github/singals/algo/leetcode/seventyFive/MoveZeroesTest.java
@@ -0,0 +1,42 @@
+package com.github.singals.algo.leetcode.seventyFive;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MoveZeroesTest {
+    @Test
+    @DisplayName("move-zeroes-when-all-zeroes")
+    void moveZeroesWhenAllZeroes() {
+        int[] input = {0, 0, 0, 0};
+        MoveZeroes.moveZeroes(input);
+        assertArrayEquals(new int[]{0, 0, 0, 0}, input);
+
+        input = new int[]{0};
+        MoveZeroes.moveZeroes(input);
+        assertArrayEquals(new int[]{0}, input);
+    }
+
+    @Test
+    @DisplayName("move-zeroes-when-some-zeroes")
+    void moveZeroesWhenSomeZeroes() {
+        int[] input = {0, 1, 0, 3, 12};
+        MoveZeroes.moveZeroes(input);
+        assertArrayEquals(new int[]{1, 3, 12, 0, 0}, input);
+//
+        input = new int[]{0, 0, 1};
+//        int[] input = new int[]{0, 0, 1};
+        MoveZeroes.moveZeroes(input);
+        assertArrayEquals(new int[]{1, 0, 0}, input);
+    }
+
+    @Test
+    @DisplayName("move-zeroes-when-no-zeroes")
+    void moveZeroesWhenNoZeroes() {
+        int[] input = {1, 2, 3, 12};
+        MoveZeroes.moveZeroes(input);
+        assertArrayEquals(new int[]{1, 2, 3, 12}, input);
+    }
+
+}
