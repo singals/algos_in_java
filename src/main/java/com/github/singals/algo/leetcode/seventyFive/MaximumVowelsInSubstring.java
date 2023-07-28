f67918342e2583cfb50a36d8aed43893ad58c862
ssingal <singalshashank90@gmail.com>
2023-07-28 17:39:17 +0530

    solve - maximum vowels in substring

---
+++ src/main/java/com/github/singals/algo/leetcode/seventyFive/MaximumVowelsInSubstring.java
@@ -0,0 +1,32 @@
+package com.github.singals.algo.leetcode.seventyFive;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+/**
+ * Given a string s and an integer k, return the maximum number of vowel letters
+ * in any substring of s with length k.
+ */
+public class MaximumVowelsInSubstring {
+    public static int maxVowels(String s, int k) {
+        int vowelsCount = 0;
+        for (int i = 0; i < k; i++) {
+            if (isVowel(s.charAt(i))) vowelsCount++;
+        }
+        int maxVowelCount = vowelsCount;
+
+        for (int i = k; i < s.length(); i++) {
+            if (isVowel(s.charAt(i - k))) vowelsCount--;
+            if (isVowel(s.charAt(i))) vowelsCount++;
+            if (maxVowelCount < vowelsCount) maxVowelCount = vowelsCount;
+            if (maxVowelCount == k) return k;
+        }
+
+        return maxVowelCount;
+    }
+
+    private static boolean isVowel(char ch) {
+        return ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u';
+    }
+}
---
+++ src/test/java/com/github/singals/algo/leetcode/seventyFive/MaximumVowelsInSubstringTest.java
@@ -0,0 +1,55 @@
+package com.github.singals.algo.leetcode.seventyFive;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MaximumVowelsInSubstringTest {
+
+    @Test
+    @DisplayName("max-vowels-when-no-vowels")
+    void maxVowelsWhenNoVowels() {
+        int maxVowels = MaximumVowelsInSubstring.maxVowels("bcdfghjklmnpqrstvwxyz", 2);
+        assertEquals(0, maxVowels);
+
+        maxVowels = MaximumVowelsInSubstring.maxVowels("bcdfghjklmnpqrstvwxyz", 3);
+        assertEquals(0, maxVowels);
+
+        maxVowels = MaximumVowelsInSubstring.maxVowels("bcdfghjklmnpqrstvwxyz", 4);
+        assertEquals(0, maxVowels);
+
+        maxVowels = MaximumVowelsInSubstring.maxVowels("bcdfghjklmnpqrstvwxyz", 6);
+        assertEquals(0, maxVowels);
+    }
+
+    @Test
+    @DisplayName("max-vowels-when-all-vowels")
+    void maxVowelsWhenAllVowels() {
+        int maxVowels = MaximumVowelsInSubstring.maxVowels("aeiou", 2);
+        assertEquals(2, maxVowels);
+
+        maxVowels = MaximumVowelsInSubstring.maxVowels("aeiou", 3);
+        assertEquals(3, maxVowels);
+
+        maxVowels = MaximumVowelsInSubstring.maxVowels("aeiou", 4);
+        assertEquals(4, maxVowels);
+
+        maxVowels = MaximumVowelsInSubstring.maxVowels("aeiou", 5);
+        assertEquals(5, maxVowels);
+    }
+
+    @Test
+    @DisplayName("max-vowels-when-some-vowels")
+    void maxVowelsWhenSomeVowels() {
+        int maxVowels = MaximumVowelsInSubstring.maxVowels("abciiidef", 3);
+        assertEquals(3, maxVowels);
+
+        maxVowels = MaximumVowelsInSubstring.maxVowels("leetcode", 3);
+        assertEquals(2, maxVowels);
+
+        maxVowels = MaximumVowelsInSubstring.maxVowels("tryhard", 4);
+        assertEquals(1, maxVowels);
+    }
+
+}
