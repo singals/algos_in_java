84bb5de4cc73c079b9fd3e4ec0403dbc92498649
ssingal <singalshashank90@gmail.com>
2023-08-01 20:02:14 +0530

    solve - pivot index

---
+++ src/main/java/com/github/singals/algo/leetcode/seventyFive/PivotIndex.java
@@ -0,0 +1,34 @@
+package com.github.singals.algo.leetcode.seventyFive;
+
+/**
+ * Given an array of integers nums, calculate the pivot index of this array.
+ * <p>
+ * The pivot index is the index where the sum of all the numbers strictly to the
+ * left of the index is equal to the sum of all the numbers strictly to the
+ * index's right.
+ * <p>
+ * If the index is on the left edge of the array, then the left sum is 0 because
+ * there are no elements to the left. This also applies to the right edge of the array.
+ * <p>
+ * Return the leftmost pivot index. If no such index exists, return -1.
+ */
+public class PivotIndex {
+    public static int pivotIndex(int[] nums) {
+        if (nums.length == 1) return 0;
+
+        int sum = 0;
+        for (int num : nums) {
+            sum += num;
+        }
+
+        int leftSum = 0;
+        for (int i = 0; i < nums.length; i++) {
+            int num = nums[i];
+            sum -= num;
+            if (sum == leftSum) return i;
+            leftSum += num;
+        }
+
+        return -1;
+    }
+}
---
+++ src/test/java/com/github/singals/algo/leetcode/seventyFive/PivotIndexTest.java
@@ -0,0 +1,36 @@
+package com.github.singals.algo.leetcode.seventyFive;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PivotIndexTest {
+
+    @Test
+    @DisplayName("pivot-index-when-it-does-not-exist")
+    void pivotIndexWhenItDoesNotExist() {
+        int pivotIndex = PivotIndex.pivotIndex(new int[]{1, 2, 3, 4, 5});
+        assertEquals(-1, pivotIndex);
+
+        pivotIndex = PivotIndex.pivotIndex(new int[]{1, 1});
+        assertEquals(-1, pivotIndex);
+
+        pivotIndex = PivotIndex.pivotIndex(new int[]{1, 1});
+        assertEquals(-1, pivotIndex);
+    }
+
+    @Test
+    @DisplayName("pivot-index-when-it-exists")
+    void pivotIndexWhenItExists() {
+        int pivotIndex = PivotIndex.pivotIndex(new int[]{1, 7, 3, 6, 5, 6});
+        assertEquals(3, pivotIndex);
+
+        pivotIndex = PivotIndex.pivotIndex(new int[]{2, 1, -1});
+        assertEquals(0, pivotIndex);
+
+        pivotIndex = PivotIndex.pivotIndex(new int[]{1});
+        assertEquals(0, pivotIndex);
+    }
+
+}
