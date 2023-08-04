fba4e7dd80aa25d40eec2849eaf5d47c04710050
ssingal <singalshashank90@gmail.com>
2023-08-04 19:21:32 +0530

    solve - equal row and column pairs

---
+++ src/main/java/com/github/singals/algo/leetcode/seventyFive/EqualRowAndColumnPair.java
@@ -0,0 +1,51 @@
+package com.github.singals.algo.leetcode.seventyFive;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Given a 0-indexed n x n integer matrix grid, return the number of pairs (ri, cj)
+ * such that row ri and column cj are equal.
+ * <p>
+ * A row and column pair is considered equal if they contain the same elements
+ * in the same order (i.e., an equal array).
+ */
+public class EqualRowAndColumnPair {
+    public static int equalPairs(int[][] grid) {
+        Map<String, Integer> rows = new HashMap<>();
+        Map<String, Integer> columns = new HashMap<>();
+
+        for (int[] row : grid) {
+            StringBuilder sb = new StringBuilder();
+            for (int num : row) {
+                sb.append(num).append('-');
+            }
+            String rowName = sb.toString();
+            Integer rowCount = rows.get(rowName);
+            if (rowCount == null) rows.put(rowName, 1);
+            else rows.put(rowName, ++rowCount);
+        }
+
+        for (int i = 0; i < grid.length; i++) {
+            StringBuilder sb = new StringBuilder();
+            for (int[] ints : grid) {
+                sb.append(ints[i]).append('-');
+            }
+            String columnName = sb.toString();
+            Integer colCount = columns.get(columnName);
+            if (colCount == null) columns.put(columnName, 1);
+            else columns.put(columnName, ++colCount);
+        }
+
+        int pairCount = 0;
+
+        for (String rowName : rows.keySet()) {
+            Integer columnCount = columns.get(rowName);
+            if (columnCount != null) {
+                pairCount += columnCount * rows.get(rowName);
+            }
+        }
+        return pairCount;
+    }
+}
---
+++ src/test/java/com/github/singals/algo/leetcode/seventyFive/EqualRowAndColumnPairTest.java
@@ -0,0 +1,35 @@
+package com.github.singals.algo.leetcode.seventyFive;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class EqualRowAndColumnPairTest {
+    @Test
+    @DisplayName("equal-pairs-when-no-pair-is-equal")
+    void equalPairsWhenNoPairIsEqual() {
+        int res = EqualRowAndColumnPair.equalPairs(new int[][]{{1, 2, 3}, {4, 5, 6}, {7, 8, 9}});
+        assertEquals(0, res);
+    }
+
+    @Test
+    @DisplayName("equal-pairs-when-some-pairs-are-equal")
+    void equalPairsWhenSomePairsAreEqual() {
+        int res = EqualRowAndColumnPair.equalPairs(new int[][]{{3, 2, 1}, {1, 7, 6}, {2, 7, 7}});
+        assertEquals(1, res);
+
+        res = EqualRowAndColumnPair.equalPairs(new int[][]{{3, 1, 2, 2}, {1, 4, 4, 5}, {2, 4, 2, 2}, {2, 4, 2, 2}});
+        assertEquals(3, res);
+    }
+
+    @Test
+    @DisplayName("equal-pairs-when-all-pairs-are-equal")
+    void equalPairsWhenAllPairsAreEqual() {
+        int res = EqualRowAndColumnPair.equalPairs(new int[][]{{1, 1, 1}, {1, 1, 1}, {1, 1, 1}});
+        assertEquals(9, res);
+
+        res = EqualRowAndColumnPair.equalPairs(new int[][]{{8, 8}, {8, 8}});
+        assertEquals(4, res);
+    }
+}
