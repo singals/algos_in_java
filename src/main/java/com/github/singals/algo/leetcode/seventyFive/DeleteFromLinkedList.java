e53a60b622af7cadaaa7ea12a8a57ece443daa9d
ssingal <singalshashank90@gmail.com>
2023-08-11 17:57:33 +0530

    solve - delete from linked list

---
+++ src/main/java/com/github/singals/algo/leetcode/seventyFive/DeleteFromLinkedList.java
@@ -0,0 +1,34 @@
+package com.github.singals.algo.leetcode.seventyFive;
+
+/**
+ * You are given the head of a linked list. Delete the middle node, and return the
+ * head of the modified linked list.
+ *
+ * The middle node of a linked list of size n is the ⌊n / 2⌋th node from the start
+ * using 0-based indexing, where ⌊x⌋ denotes the largest integer less than or equal
+ * to x.
+ *
+ *     For n = 1, 2, 3, 4, and 5, the middle nodes are 0, 1, 1, 2, and 2, respectively.
+ */
+public class DeleteFromLinkedList {
+    public ListNode deleteMiddle(ListNode head) {
+        if (head.next == null) return null;
+        ListNode ptr = head;
+        int n = 1;
+        while (ptr.next != null) {
+            ptr = ptr.next;
+            n++;
+        }
+        int mid = n / 2;
+        n = 1;
+        ptr = head;
+        while (n < mid) {
+            ptr = ptr.next;
+            n++;
+        }
+
+        if (ptr.next != null) ptr.next = ptr.next.next;
+        return head;
+    }
+}
+
---
+++ src/test/java/com/github/singals/algo/leetcode/seventyFive/DeleteFromLinkedListTest.java
@@ -0,0 +1,68 @@
+package com.github.singals.algo.leetcode.seventyFive;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DeleteFromLinkedListTest {
+    @Test
+    @DisplayName("delete-middle-example-one")
+    void deleteMiddleExampleOne() {
+        ListNode node7 = new ListNode(6);
+        ListNode node6 = new ListNode(2, node7);
+        ListNode node5 = new ListNode(1, node6);
+        ListNode node4 = new ListNode(7, node5);
+        ListNode node3 = new ListNode(4, node4);
+        ListNode node2 = new ListNode(3, node3);
+        ListNode node1 = new ListNode(1, node2);
+
+        DeleteFromLinkedList objUnderTest = new DeleteFromLinkedList();
+        ListNode res = objUnderTest.deleteMiddle(node1);
+
+        assertEquals(1, res.val);
+        res = res.next;
+        assertEquals(3, res.val);
+        res = res.next;
+        assertEquals(4, res.val);
+        res = res.next;
+        assertEquals(1, res.val);
+        res = res.next;
+        assertEquals(2, res.val);
+        res = res.next;
+        assertEquals(6, res.val);
+        assertNull(res.next);
+    }
+
+    @Test
+    @DisplayName("delete-middle-example-two")
+    void deleteMiddleExampleTwo() {
+        ListNode node4 = new ListNode(4);
+        ListNode node3 = new ListNode(3, node4);
+        ListNode node2 = new ListNode(2, node3);
+        ListNode node1 = new ListNode(1, node2);
+
+        DeleteFromLinkedList objUnderTest = new DeleteFromLinkedList();
+        ListNode res = objUnderTest.deleteMiddle(node1);
+
+        assertEquals(1, res.val);
+        res = res.next;
+        assertEquals(2, res.val);
+        res = res.next;
+        assertEquals(4, res.val);
+        assertNull(res.next);
+    }
+
+    @Test
+    @DisplayName("delete-middle-example-three")
+    void deleteMiddleExampleThree() {
+        ListNode node2 = new ListNode(1);
+        ListNode node1 = new ListNode(2, node2);
+
+        DeleteFromLinkedList objUnderTest = new DeleteFromLinkedList();
+        ListNode res = objUnderTest.deleteMiddle(node1);
+
+        assertEquals(2, res.val);
+        assertNull(res.next);
+    }
+}
