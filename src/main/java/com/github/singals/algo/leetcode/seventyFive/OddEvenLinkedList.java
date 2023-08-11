b93629a4c6886b21b203f6a58153a10b411dcd3e
ssingal <singalshashank90@gmail.com>
2023-08-11 17:57:53 +0530

    solve - odd even linked list

---
+++ src/main/java/com/github/singals/algo/leetcode/seventyFive/OddEvenLinkedList.java
@@ -0,0 +1,45 @@
+package com.github.singals.algo.leetcode.seventyFive;
+
+/**
+ * Given the head of a singly linked list, group all the nodes with odd indices
+ * together followed by the nodes with even indices, and return the reordered list.
+ * <p>
+ * The first node is considered odd, and the second node is even, and so on.
+ * <p>
+ * Note that the relative order inside both the even and odd groups should remain
+ * as it was in the input.
+ * <p>
+ * You must solve the problem in O(1) extra space complexity and O(n) time complexity.
+ */
+public class OddEvenLinkedList {
+
+    public static ListNode oddEvenList(ListNode head) {
+        if (head == null || head.next == null) return head;
+        ListNode res = new ListNode();
+        ListNode resPtr = res;
+        ListNode ptr = head;
+        //add odd nodes first
+        while (ptr != null) {
+            if (ptr.next == null) {
+                resPtr.next = new ListNode(ptr.val, null);
+                ptr = null;
+            } else {
+                resPtr.next = new ListNode(ptr.val, ptr.next.next);
+                ptr = ptr.next.next;
+            }
+            resPtr = resPtr.next;
+        }
+        ptr = head.next;
+        while (ptr != null) {
+            if (ptr.next == null) {
+                resPtr.next = new ListNode(ptr.val, null);
+                ptr = null;
+            } else {
+                resPtr.next = new ListNode(ptr.val, ptr.next.next);
+                ptr = ptr.next.next;
+            }
+            resPtr = resPtr.next;
+        }
+        return res.next;
+    }
+}
---
+++ src/test/java/com/github/singals/algo/leetcode/seventyFive/OddEvenLinkedListTest.java
@@ -0,0 +1,59 @@
+package com.github.singals.algo.leetcode.seventyFive;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.w3c.dom.NodeList;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class OddEvenLinkedListTest {
+    @Test
+    @DisplayName("odd-even-list-example-one")
+    void oddEvenListExampleOne() {
+        ListNode node5 = new ListNode(5);
+        ListNode node4 = new ListNode(4, node5);
+        ListNode node3 = new ListNode(3, node4);
+        ListNode node2 = new ListNode(2, node3);
+        ListNode node1 = new ListNode(1, node2);
+
+        ListNode res = OddEvenLinkedList.oddEvenList(node1);
+        assertEquals(1, res.val);
+        res = res.next;
+        assertEquals(3, res.val);
+        res = res.next;
+        assertEquals(5, res.val);
+        res = res.next;
+        assertEquals(2, res.val);
+        res = res.next;
+        assertEquals(4, res.val);
+        assertNull(res.next);
+    }
+
+    @Test
+    @DisplayName("odd-even-list-example-two")
+    void oddEvenListExampleTwo() {
+        ListNode node7 = new ListNode(7);
+        ListNode node6 = new ListNode(4, node7);
+        ListNode node5 = new ListNode(6, node6);
+        ListNode node4 = new ListNode(5, node5);
+        ListNode node3 = new ListNode(3, node4);
+        ListNode node2 = new ListNode(1, node3);
+        ListNode node1 = new ListNode(2, node2);
+
+        ListNode res = OddEvenLinkedList.oddEvenList(node1);
+        assertEquals(2, res.val);
+        res = res.next;
+        assertEquals(3, res.val);
+        res = res.next;
+        assertEquals(6, res.val);
+        res = res.next;
+        assertEquals(7, res.val);
+        res = res.next;
+        assertEquals(1, res.val);
+        res = res.next;
+        assertEquals(5, res.val);
+        res = res.next;
+        assertEquals(4, res.val);
+        assertNull(res.next);
+    }
+}
