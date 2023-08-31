ffeede250a53ce414b7310f531a2c8458b01b38c
ssingal <singalshashank90@gmail.com>
2023-08-31 20:10:24 +0530

    solve - right side view

--- src/main/java/com/github/singals/algo/leetcode/seventyFive/RightSideView.java
+++ src/main/java/com/github/singals/algo/leetcode/seventyFive/RightSideView.java
@@ -1,9 +1,7 @@
 package com.github.singals.algo.leetcode.seventyFive;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 public class RightSideView {
     private List<Integer> res = new ArrayList<>();
