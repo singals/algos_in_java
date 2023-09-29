8d44a9a7bba95f6951c395f39f344adae4f77511
Shashank Singal <singalshashank90@gmail.com>
2023-09-29 20:35:25 +0530

    solve - delete node from bst

---
+++ src/main/java/com/github/singals/algo/leetcode/seventyFive/DeleteFromBst.java
@@ -0,0 +1,37 @@
+package com.github.singals.algo.leetcode.seventyFive;
+
+/**
+ * Given a root node reference of a BST and a key, delete the node with the given
+ * key in the BST. Return the root node reference (possibly updated) of the BST.
+ * <p>
+ * Basically, the deletion can be divided into two stages:
+ * - Search for a node to remove.
+ * - If the node is found, delete the node.
+ */
+public class DeleteFromBst {
+
+    public TreeNode deleteNode(TreeNode root, int key) {
+        if (root == null) return null;
+
+        if (key > root.val) {
+            root.right = deleteNode(root.right, key);
+        } else if (key < root.val) {
+            root.left = deleteNode(root.left, key);
+        } else {
+            if (root.left == null && root.right == null) return null;
+            else if (root.left == null) return root.right;
+            else if (root.right == null) return root.left;
+            else {
+                TreeNode ptr = getLeastValueFromBst(root.right);
+                root.val = ptr.val;
+                root.right = deleteNode(root.right, root.val);
+            }
+        }
+        return root;
+    }
+
+    private TreeNode getLeastValueFromBst(TreeNode node) {
+        while (node.left != null) node = node.left;
+        return node;
+    }
+}
---
+++ src/test/java/com/github/singals/algo/leetcode/seventyFive/DeleteFromBstTest.java
@@ -0,0 +1,93 @@
+package com.github.singals.algo.leetcode.seventyFive;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DeleteFromBstTest {
+
+    private DeleteFromBst deleteFromBST;
+    private TreeNode root;
+    private TreeNode newTree;
+
+    @BeforeEach
+    void setUp() {
+        deleteFromBST = new DeleteFromBst();
+        TreeNode l3Node1 = new TreeNode(1);
+        TreeNode l3Node2 = new TreeNode(5);
+        TreeNode l3Node3 = new TreeNode(7);
+        TreeNode l3Node4 = new TreeNode(13);
+        TreeNode l2Node1 = new TreeNode(2, l3Node1, null);
+        TreeNode l2Node2 = new TreeNode(6, l3Node2, l3Node3);
+        TreeNode l2Node3 = new TreeNode(9);
+        TreeNode l2Node4 = new TreeNode(11, null, l3Node4);
+        TreeNode l1Node1 = new TreeNode(4, l2Node1, l2Node2);
+        TreeNode l1Node2 = new TreeNode(10, l2Node3, l2Node4);
+        root = new TreeNode(8, l1Node1, l1Node2);
+    }
+
+    @AfterEach
+    void tearDown() {
+        newTree = null;
+    }
+
+    @Test
+    void testDeleteRootNode() {
+        TreeNode newTree = deleteFromBST.deleteNode(root, 8);
+
+        assertEquals(9, newTree.val);
+        assertEquals(root.left, newTree.left);
+
+        TreeNode rightChild = newTree.right;
+        assertEquals(10, rightChild.val);
+
+        assertNull(rightChild.left);
+        assertEquals(11, rightChild.right.val);
+
+        assertNull(rightChild.right.left);
+        assertEquals(13, rightChild.right.right.val);
+    }
+
+    @Test
+    void testDeleteNodeWhenNodeDoesNotExist() {
+        TreeNode newTree = deleteFromBST.deleteNode(root, 27);
+        assertEquals(root, newTree);
+    }
+
+    @Test
+    void testDeleteNodeWhenNodeDoesIsLeaf() {
+        TreeNode newTree = deleteFromBST.deleteNode(root, 5);
+
+        assertEquals(root.right, newTree.right);
+        assertEquals(root.left.val, newTree.left.val);
+        assertEquals(root.left.left, newTree.left.left);
+
+        assertEquals(root.left.right.val, newTree.left.right.val);
+        assertEquals(root.left.right.right.val, newTree.left.right.right.val);
+        assertNull(root.left.right.left);
+    }
+
+    @Test
+    void testDeleteNodeWhenNodeOnlyHasLeftChild() {
+        TreeNode newTree = deleteFromBST.deleteNode(root, 2);
+
+        assertEquals(root.right, newTree.right);
+        assertEquals(root.left.val, newTree.left.val);
+        assertEquals(root.left.right, newTree.left.right);
+
+        assertEquals(1, newTree.left.left.val);
+    }
+
+    @Test
+    void testDeleteNodeWhenNodeOnlyHasRightChild() {
+        TreeNode newTree = deleteFromBST.deleteNode(root, 11);
+
+        assertEquals(root.left, newTree.left);
+        assertEquals(root.right.val, newTree.right.val);
+        assertEquals(root.right.left, newTree.right.left);
+
+        assertEquals(13, newTree.right.right.val);
+    }
+}
