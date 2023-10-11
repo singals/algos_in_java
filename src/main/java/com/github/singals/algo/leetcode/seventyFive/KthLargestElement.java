229f20b06b3ee195e485bf8e43db81fe8a5065b2
Shashank Singal <singalshashank90@gmail.com>
2023-10-11 20:12:11 +0530

    solve - kth largest element - heap/priority-queue

---
+++ src/main/java/com/github/singals/algo/leetcode/seventyFive/KthLargestElement.java
@@ -0,0 +1,23 @@
+package com.github.singals.algo.leetcode.seventyFive;
+
+import java.util.PriorityQueue;
+
+/**
+ * Given an integer array nums and an integer k, return the kth largest element in the array.
+ * <p>
+ * Note that it is the kth largest element in the sorted order, not the kth distinct element.
+ * <p>
+ * Can you solve it without sorting?
+ */
+public class KthLargestElement {
+    public int findKthLargest(int[] nums, int k) {
+        PriorityQueue<Integer> queue = new PriorityQueue<>();
+        for (int num : nums) queue.add(num);
+        int mid = queue.size() - k;
+        for (int i = 0; i < mid; i++) {
+            queue.poll();
+        }
+        return queue.poll();
+    }
+
+}
---
+++ src/test/java/com/github/singals/algo/leetcode/seventyFive/KthLargestElementTest.java
@@ -0,0 +1,33 @@
+package com.github.singals.algo.leetcode.seventyFive;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class KthLargestElementTest {
+
+    private KthLargestElement kthLargestElement;
+
+    @BeforeEach
+    void setUp() {
+        kthLargestElement = new KthLargestElement();
+    }
+
+    @Test
+    @DisplayName("test-find-kth-largest-example-one")
+    void testFindKthLargestExampleOne() {
+        int res = kthLargestElement.findKthLargest(new int[]{3, 2, 1, 5, 6, 4}, 2);
+
+        assertEquals(5, res);
+    }
+
+    @Test
+    @DisplayName("test-find-kth-largest-example-two")
+    void testFindKthLargestExampleTwo() {
+        int res = kthLargestElement.findKthLargest(new int[]{3, 2, 3, 1, 2, 4, 5, 5, 6}, 4);
+
+        assertEquals(4, res);
+    }
+}
