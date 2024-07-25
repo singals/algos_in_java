1cd20ad14168a6f37e4ec98d9f1dea252def3c19
Shashank Singal <singalshashank90@gmail.com>
2024-07-25 22:57:07 +0530

    solve - successful pairs of spells and potions - binary search

---
+++ src/main/java/com/github/singals/algo/leetcode/seventyFive/SuccessfulPairsOfSpellsAndPotion.java
@@ -0,0 +1,43 @@
+package com.github.singals.algo.leetcode.seventyFive;
+
+import java.util.Arrays;
+
+// https://leetcode.com/problems/successful-pairs-of-spells-and-potions/description/
+public class SuccessfulPairsOfSpellsAndPotion {
+
+    public static int[] successfulPairs(int[] spells, int[] potions, long success) {
+        int n = spells.length;
+        int m = potions.length;
+        int[] res = new int[n];
+        if (success == 1) {
+            for (int i = 0; i < n; i++) {
+                res[i] = m;
+            }
+        } else {
+            Arrays.sort(potions);
+            for (int i = 0; i < n; i++) {
+                res[i] = findSuccesses(spells[i], potions, success, 0, m - 1);
+            }
+        }
+        return res;
+    }
+
+    private static int findSuccesses(int spell, int[] potions, long success, int startInd, int endInd) {
+        if (success > ((long) spell * potions[endInd])) return 0;
+        else if (success <= ((long) spell * potions[0])) return potions.length - startInd;
+        else if (endInd - startInd == 1 && (success <= ((long) spell * potions[endInd]))) {
+            return potions.length - endInd;
+        } else {
+            int mid = (startInd + endInd) / 2;
+            if (success > ((long) spell * potions[mid])) {
+                //navigate right
+                startInd = mid;
+            } else {
+                //success <= ((long) spell * potions[mid]
+                //navigate left
+                endInd = mid;
+            }
+            return findSuccesses(spell, potions, success, startInd, endInd);
+        }
+    }
+}
---
+++ src/test/java/com/github/singals/algo/leetcode/seventyFive/SuccessfulPairsOfSpellsAndPotionTest.java
@@ -0,0 +1,42 @@
+package com.github.singals.algo.leetcode.seventyFive;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class SuccessfulPairsOfSpellsAndPotionTest {
+
+    private int[] spells;
+    private int[] potions;
+    private long success;
+    private int[] actualResult;
+    private int[] expectedResult;
+
+    @Test
+    @DisplayName("successful-pairs-of-spells-and-potions-test-one")
+    void successfulPairsOfSpellsAndPotionsTestOne() {
+        spells = new int[]{5, 1, 3};
+        potions = new int[]{1, 2, 3, 4, 5};
+        success = 7;
+        expectedResult = new int[]{4, 0, 3};
+
+        actualResult = SuccessfulPairsOfSpellsAndPotion.successfulPairs(spells, potions, success);
+
+        assertArrayEquals(expectedResult, actualResult);
+    }
+
+    @Test
+    @DisplayName("successful-pairs-of-spells-and-potions-test-two")
+    void successfulPairsOfSpellsAndPotionsTestTwo() {
+        spells = new int[]{3, 1, 2};
+        potions = new int[]{8, 5, 8};
+        success = 16;
+        expectedResult = new int[]{2, 0, 2};
+
+        actualResult = SuccessfulPairsOfSpellsAndPotion.successfulPairs(spells, potions, success);
+
+        assertArrayEquals(expectedResult, actualResult);
+    }
+
+}
